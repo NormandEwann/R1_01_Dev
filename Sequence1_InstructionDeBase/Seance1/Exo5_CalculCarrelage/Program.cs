11b2dee94473a0bbd7d032282b95cfa88ec9f2e4
NormandEwann <ewann.normand@etu.univ-smb.fr>
2026-09-19 00:19:57 +0200

    Refactor Program.cs with comments for clarity
    Added comments for clarity and organization.

--- Sequence1_InstructionDeBase/Seance1/Exo5_CalculCarrelage/Program.cs
+++ Sequence1_InstructionDeBase/Seance1/Exo5_CalculCarrelage/Program.cs
@@ -6,7 +6,10 @@ namespace Exo5_CalculCarrelage
     {
         static void Main(string[] args)
         {
+            //en-tête d'encodage UTF8
             Console.OutputEncoding = Encoding.UTF8;
+
+            //création des variables
             double largeurSol;
             double longueurSol;
             double largeurCar;
@@ -16,25 +19,36 @@ namespace Exo5_CalculCarrelage
             double nbCarrelage;
             double quantPaquet;
             double nbPaquet;
+
+            //attribution de valeurs aux variables
             Console.WriteLine("CALCUL DU NOMBRE DE PAQUETS TOTAL");
             Console.WriteLine("----------------------------------");
             Console.WriteLine("Largeur du sol (m) :");
             largeurSol = double.Parse(Console.ReadLine());
+            
             Console.WriteLine("Longueur du sol (m) :");
             longueurSol = double.Parse(Console.ReadLine());
+            
             Console.WriteLine("----------------------------------");
             Console.WriteLine("Largeur du carrelage (cm) :");
             largeurCar = double.Parse(Console.ReadLine());
+            
             Console.WriteLine("Longueur du carrelage (cm) :");
             longueurCar = double.Parse(Console.ReadLine());
+            
             Console.WriteLine("Nombre de Carrelage par Paquets");
             quantPaquet = int.Parse(Console.ReadLine());
+
+            //maths | Ici je me sert de "temp" comme variable temporaire et faire mes calculs.
             tempLargeur = (largeurSol * 100) / largeurCar;
             tempLargeur = Math.Ceiling(tempLargeur);
             tempLongueur = (longueurSol * 100) / longueurCar;
             tempLongueur = Math.Ceiling(tempLongueur);
+            
             nbCarrelage = tempLargeur * tempLongueur;
             nbPaquet = Math.Ceiling(nbPaquet = nbCarrelage / quantPaquet);
+
+            //afficher le résultat
             Console.WriteLine("Il faudra " + nbPaquet + " paquets de carrelage.");
         }
     }
