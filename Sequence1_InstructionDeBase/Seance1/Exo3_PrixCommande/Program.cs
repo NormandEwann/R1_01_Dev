aa11631d1f94d09b3bdaef8fd900b8f637bbf958
NormandEwann <ewann.normand@etu.univ-smb.fr>
2026-09-19 00:08:26 +0200

    Improve variable naming and add comments in Program.cs
    Refactor variable names and add comments for clarity.

--- Sequence1_InstructionDeBase/Seance1/Exo3_PrixCommande/Program.cs
+++ Sequence1_InstructionDeBase/Seance1/Exo3_PrixCommande/Program.cs
@@ -4,25 +4,35 @@ namespace Exo3_PrixCommande
 {
     internal class Program
     {
+        //attribution de variables statiques
         static readonly decimal TVA = 0.20m;
         
         static void Main(string[] args)
         {
+            //en-tete pour encoder en UTF8 (écrire "€")
             Console.OutputEncoding = Encoding.UTF8;
+
+            //création des variables
             decimal prixHT;
-            decimal total;
             decimal prixTTC;
-            int quant;
-            String nom;
+            decimal prixTotal;
+            int quantité;
+
+            //atribution des variables
             Console.WriteLine("CALCUL DE LA TAXE ET DU PRIX TOTAL");
             Console.WriteLine("----------------------------------");
             Console.WriteLine("Prix Hors Taxes :");
             prixHT = decimal.Parse(Console.ReadLine());
+            
             Console.WriteLine("Nombre d'objets :");
             quant = int.Parse(Console.ReadLine());
+
+            //Calculs            
             prixTTC = prixHT * (1 + TVA);
             prixTTC = Math.Round(prixTTC, 2);
-            total = prixTTC * quant;
+            prixTotal = prixTTC * quantité;
+            
+            //Afficher les résultats
             Console.WriteLine("Le prix de l'objet toutes taxes comprises est de " + prixTTC + "€ et votre total est de " + total + "€!");
         }
     }
