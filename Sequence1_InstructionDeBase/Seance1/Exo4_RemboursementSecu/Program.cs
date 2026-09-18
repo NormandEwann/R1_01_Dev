b027bd2e6cbdd9ef44f2a2b62a878dbc1ea64763
NormandEwann <ewann.normand@etu.univ-smb.fr>
2026-09-19 00:15:42 +0200

    Refactor reimbursement calculation program
    Refactor code to use a constant for charges and improve comments.

--- Sequence1_InstructionDeBase/Seance1/Exo4_RemboursementSecu/Program.cs
+++ Sequence1_InstructionDeBase/Seance1/Exo4_RemboursementSecu/Program.cs
@@ -4,26 +4,39 @@ namespace Exo4_RemboursementSecu
 {
     internal class Program
     {
+        //création de variables statiques
         static readonly decimal TAUXSECU = 0.70m;
         static readonly decimal TAUXMUTUELLE = 0.30m;
+        static readonly int CHARGES = 2;
+        
         static void Main(string[] args)
         {
+            //en-tête d'encodage en UTF8
             Console.OutputEncoding = Encoding.UTF8;
+
+            //création des variables
             decimal prixBase;
             decimal prixCharges;
             decimal secu;
             decimal mutuelle;
+
+            //attribution de valeurs aux variables
             Console.WriteLine("CALCUL DE LA SECU ET MUTUELLE");
             Console.WriteLine("----------------------------------");
             Console.WriteLine("Prix Payé en Consultation :");
             prixBase = decimal.Parse(Console.ReadLine());
-            prixCharges = prixBase - 2;
-            Console.WriteLine("Prix sans charges : " + prixCharges);
+            
+            //Maths            
+            prixCharges = prixBase - CHARGES;
             secu = prixCharges * TAUXSECU;
-            secu = Math.Round(secu, 2);
             mutuelle = prixCharges * TAUXMUTUELLE;
+            
+            //Arrondir avec 2 chiffres après la virgule
+            secu = Math.Round(secu, 2);
             mutuelle = Math.Round(mutuelle, 2);
-            Console.WriteLine("Le remboursement de la sécu est de " + secu + "€ et la mutuelle rembourse " + mutuelle + "€!");
+
+            //Afficher le résultat
+            Console.WriteLine("Le prix sans charges est de " + prixCharges + " €.\nLe remboursement de la sécu est de " + secu + "€ et la mutuelle rembourse " + mutuelle + "€!");
         }
     }
 }
