ba1373ab9382de65a1be9bbbc17764486e004da7
NormandEwann <ewann.normand@etu.univ-smb.fr>
2026-09-19 14:42:29 +0200

    Enhance Program.cs with comments for clarity
    Added comments to clarify variable creation, assignment, and calculations.

--- Sequence1_InstructionDeBase/Seance2/Exo6_Pythagore/Program.cs
+++ Sequence1_InstructionDeBase/Seance2/Exo6_Pythagore/Program.cs
@@ -6,20 +6,24 @@ namespace Exo6_Pythagore
     {
         static void Main(string[] args)
         {
-            Console.OutputEncoding = Encoding.UTF8;
-            double longueurEchelle = 0, hauteur = 0;
+            //Creation des variables
+            double longueurEchelle = 0, hauteur = 0; ecartement = 0;
+
+            //Attribution des variables
             Console.WriteLine("Longueur Echelle en mètres :");
             longueurEchelle = double.Parse(Console.ReadLine());
 
             Console.WriteLine("Hauteur à atteindre en mètres :");
             hauteur = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("------------------------");
-
-            double ecartement = Math.Pow(longueurEchelle,2) - Math.Pow(hauteur,2);
+            //Maths (pythagore : a² + b² = c², donc a² = c² - b²)
+            //Math.Pow permet de faire des puissances, Math.Sqrt permet de faire la racine carré, Math.Round permet d'arrondir à 2 chiffres après la virgule
+            ecartement = Math.Pow(longueurEchelle,2) - Math.Pow(hauteur,2);
             ecartement = Math.Sqrt(ecartement);
             ecartement = Math.Round(ecartement, 2);
 
+            //Affichage du résultat
+            Console.WriteLine("------------------------");
             Console.WriteLine("Il faut espacer le pied de l'échelle de " + ecartement + "m pour atteindre " + hauteur + "m de haut.");
         }
     }
