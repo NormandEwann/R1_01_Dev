c6f494723c9e15806d5cc92981c803b2175f26ac
NormandEwann <ewann.normand@etu.univ-smb.fr>
2026-09-19 14:21:13 +0200

    Enhance Program.cs with comments for clarity
    Added comments to clarify variable creation, assignment, and calculation.

--- Sequence1_InstructionDeBase/Seance2/Exo3_ConversionCelsiusFahrenheit/Program.cs
+++ Sequence1_InstructionDeBase/Seance2/Exo3_ConversionCelsiusFahrenheit/Program.cs
@@ -4,10 +4,19 @@
     {
         static void Main(string[] args)
         {
+            //Création des variables
             double celsius = 0;
+
+            //Attribution de la variable
             Console.WriteLine("Valeur en Celsius :");
             celsius = double.Parse(Console.ReadLine());
+
+            //Maths (Fahrenheit = 32 + 1,8*celsius)
+            //Attention, j'ai fait mon calcul dans "Math.Round(variable,2)" 
+            //pour arrondir directement ma variable a 2 chiffres après la virgule en une ligne.
             double fahren = Math.Round(((1.8 * celsius) + 32),2);
+
+            //Afficher le résultat
             Console.WriteLine("------------------------");
             Console.WriteLine("La température en Fahrenheit est de : " + fahren + "°F.");
         }
