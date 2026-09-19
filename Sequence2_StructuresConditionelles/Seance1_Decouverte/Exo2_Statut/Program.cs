83f37636d366179f751dee48b2465fadb8599aa0
NormandEwann <ewann.normand@etu.univ-smb.fr>
2026-09-19 15:08:44 +0200

    Enhance Program.cs with explanatory comments
    Added comments to explain user input handling and age classification logic.

--- Sequence2_StructuresConditionelles/Seance1_Decouverte/Exo2_Statut/Program.cs
+++ Sequence2_StructuresConditionelles/Seance1_Decouverte/Exo2_Statut/Program.cs
@@ -4,8 +4,16 @@
     {
         static void Main(string[] args)
         {
+            //Création et attribution directe de la valeur a la variable
             Console.WriteLine("Age de l'utilisateur :");
             string input = Console.ReadLine();
+
+            //Comme pour l"exercice 1, on vérifie que l'utilisateur a bien entré un nombre *et* que ce nombre est positif
+            //On utilise TryParse pour convertir la valeur entrée string en int, et on vérifie que le résultat est supérieur ou égal à 0
+            //De plus, cette opéartion ressort la valeur dans une variable age, que l'on peut ensuite utiliser pour les conditions suivantes (out int age)
+
+            //On utilise une structure if/else if/else pour vérifier les différentes conditions et afficher le statut correspondant
+            //Sachant que les contitions sont en cascade, pas besoin de vérifier que l'age est supérieur à 9 pour la condition "Pré-Ado", car si l'age est inférieur ou égal à 9, le programme ne rentrera pas dans cette condition etc.
 
             if (!int.TryParse(input, out int age) || age < 0)
             {
