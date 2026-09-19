94f602953549149bcaadb5ff55975ab21a2cbf66
NormandEwann <ewann.normand@etu.univ-smb.fr>
2026-09-19 15:54:33 +0200

    Enhance Program.cs with comments for clarity
    Added comments to explain variable creation, user input validation, and operation execution.

--- Sequence2_StructuresConditionelles/Seance2_Approfondissement/Exo4_Calculatrice/Program.cs
+++ Sequence2_StructuresConditionelles/Seance2_Approfondissement/Exo4_Calculatrice/Program.cs
@@ -4,11 +4,22 @@
     {
         static void Main(string[] args)
         {
+            //Creation de variables puis affectation de valeur
             double result = 0;
             Console.WriteLine(
-                $"----------------------------------\n MENU\n----------------------------------\n+ pour additionner\n- pour soustraire\n* pour multiplier\n/ pour diviser\n----------------------------------");
+                $"----------------------------------\n" +
+                $"MENU\n" +
+                $"----------------------------------\n" +
+                $"+ pour additionner\n" +
+                $"- pour soustraire\n" +
+                $"* pour multiplier\n" +
+                $"/ pour diviser\n" +
+                $"----------------------------------");
             string operateur = Console.ReadLine()?.Trim();
 
+            //On test si l'utilisateur a entré un opérateur valide grace a un TryParse, si ce n'est pas le cas on affiche un message d'erreur et on quitte le programme
+            //Rappel : TryParse permet de tester si une valeur peut être convertie en un type spécifique, ici double. Si la conversion échoue, il retourne false.
+            //Puis il retorune la valeur convertie dans la variable double A (pour nombre 1) ou B (pour nombre 2) grace a "out double".
             Console.Write("Nombre 1 : ");
             if (!double.TryParse(Console.ReadLine(), out double A))
             {
@@ -22,6 +33,7 @@
                 return;
             }
 
+            //Conditionnel avec switch pour effectuer l'opération en fonction de l'opérateur entré par l'utilisateur
             switch (operateur)
             {
                 case "+":
@@ -49,10 +61,11 @@
                     break;
 
                 default:
-                    Console.WriteLine("Erreur, composant invalide, veuillez entrer +, _, * ou /.");
+                    Console.WriteLine("Erreur, composant invalide, veuillez entrer +, -, * ou /.");
                     break;
             }
 
+            //Affichage du résultat
             Console.WriteLine($"Le résultat est {result}.");
         }
     }
