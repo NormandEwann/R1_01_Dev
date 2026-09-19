73fc73ced738d00946e7756daf97493729f4e838
NormandEwann <ewann.normand@etu.univ-smb.fr>
2026-09-19 15:45:00 +0200

    Enhance Program.cs with comments for better understanding
    Added comments for clarity and organization.

--- Sequence2_StructuresConditionelles/Seance2_Approfondissement/Exo2_TarificationAggressive/Program.cs
+++ Sequence2_StructuresConditionelles/Seance2_Approfondissement/Exo2_TarificationAggressive/Program.cs
@@ -4,19 +4,29 @@ namespace Exo2_TarificationAggressive
 {
     internal class Program
     {
+        //Création des variables statiques
         static readonly double QT_MOINS_10 = 149.99, QT_10_49 = 134.99, QT_PLUS_49 = 109.99;
         static readonly int PETITE_QUANTITE = 10, GRANDE_QUANTITE = 49;
+
         static void Main(string[] args)
         {
+            //encodage en UTF8
             Console.OutputEncoding = Encoding.UTF8;
+
+            //Variables et attribution de valeurs
             double prix = 0;
             Console.WriteLine("Quantité ?");
             string input = (Console.ReadLine());
+
+            //Conditionnel rapide pour vérifier si l'entrée est un nombre valide et calculer le prix en fonction de la quantité
             if (!int.TryParse(input, out int quantite) || quantite < 0)
             {
                 Console.WriteLine("Veuillez choisir un nombre valide");
                 return;
+                //return pour stopper l'exécution du programme
             }
+
+            //Conditionnel
             else if (quantite < PETITE_QUANTITE)
             {
                 prix = quantite*QT_MOINS_10;
@@ -30,6 +40,7 @@ namespace Exo2_TarificationAggressive
                 prix = quantite * QT_PLUS_49;
             }
 
+            //Afficher les réultats
             Console.WriteLine($"Le tarif est de {prix:C}.");
         }
     }
