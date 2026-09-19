205e562c0c161bb252f556c1308ef5f45d017219
NormandEwann <ewann.normand@etu.univ-smb.fr>
2026-09-19 14:44:37 +0200

    Refactor Program.cs and add comments for clarity
    Refactor code for clarity and add comments.

--- Sequence1_InstructionDeBase/Seance2/Exo7_InteretsBancaire/Program.cs
+++ Sequence1_InstructionDeBase/Seance2/Exo7_InteretsBancaire/Program.cs
@@ -6,23 +6,29 @@ namespace Exo7_InteretsBancaire
     {
         static void Main(string[] args)
         {
+            //Encodage en UTF8
             Console.OutputEncoding = Encoding.UTF8;
-            double depot = 0, taux = 0, annee = 0;
+
+            //Création des variables
+            double depot = 0, taux = 0, annee = 0, total = 0, gain = 0;
+
+            //Atrtribution des valeurs aux variables
             Console.WriteLine("Dépot :");
             depot = double.Parse(Console.ReadLine());
 
             Console.WriteLine("Taux  :");
             taux = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("Nb Année :");
+            Console.WriteLine("Nombre d'Année :");
             annee = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("------------------------");
-
-            double total = depot * (Math.Pow((1 + taux), annee));
+            //Maths (rappel : Math.Pow(nombre, puissance))
+            total = depot * (Math.Pow((1 + taux), annee));
             total = Math.Round(total, 2);
-            double gain = total - depot;
+            gain = total - depot;
 
+            //Affichage des résultats
+            Console.WriteLine("------------------------");
             Console.WriteLine("Dans " + annee + " ans, tu auras " + total + "€, soit un gain de " + gain + "€.");
         }
     }
