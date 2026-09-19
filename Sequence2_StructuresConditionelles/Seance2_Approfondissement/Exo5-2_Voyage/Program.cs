0bc97a23a445cc03a20f496c6c93a865baf111aa
NormandEwann <ewann.normand@etu.univ-smb.fr>
2026-09-19 16:07:16 +0200

    Enhance Program.cs with comments and documentation
    Added comments for clarity and improved code documentation.

--- Sequence2_StructuresConditionelles/Seance2_Approfondissement/Exo5-2_Voyage/Program.cs
+++ Sequence2_StructuresConditionelles/Seance2_Approfondissement/Exo5-2_Voyage/Program.cs
@@ -4,14 +4,28 @@ namespace Exo5_Voyage
 {
     internal class Program
     {
+
+        //Creation de variables statiques
         static readonly double PLAZZA_SEM = 753, PLAZZA_SUPP = 47, RIVIERA_SEM = 784, RIVIERA_SUPP = 44;
         static readonly double PLAZZA_O = 29, PLAZZA_L = 33, RIVIERA_O = 27, RIVIERA_L = 30;
         static void Main(string[] args)
         {
+            //Cet exemple de code sert à utiliser d'autres outils comme vu dans d'autres exercice. On se sert d'un opérateur ternaire pour alléger le code et remplacer les else/if
+            //Il introduit aussi une suite de conditions pour vérifier que l'utilisateur ne rentre pas de mauvaises valeurs.
+
+            //Encodage UTF8
             Console.OutputEncoding = Encoding.UTF8;
+
+            //Variables
             double prixHotel = 0, prixVoiture = 0, prixTotal = 0;
 
             //QUESTIONS ----------------------------------------------------------------------------------------------------------------------------------
+
+            //On poses nos questions et on vérifie si les réponses sont valides, sinon on affiche un message d'erreur et on quitte le programme
+            //Si nbJour est bien un nombre et = ou supérieur à 7
+            //Si hotel est bien R ou P
+            //Si vehicule est bien O ou N
+            //Et si jourVoiture est bien un nombre et inférieur ou égal à nbJour (en gros pas plus de jours de location que de jours de voyage)
 
             Console.WriteLine("Nb de jours de voyage :");
             if (!int.TryParse(Console.ReadLine(), out int nbJour))
@@ -65,6 +79,11 @@ namespace Exo5_Voyage
 
             //Conditionel------------------------------------------------------------------------------------------------------------------------------
 
+            //Une fois nos questions posées, on s'occupe du conditionnel
+
+            //Si l'hotel est le Plazza, on calcule le prix de l'hotel et de la voiture grace a des opérateurs ternaires, sinon on fait pareil pour le Riviera
+            //Rappel : une condition ternaire est une manière plus concise d'écrire un if/else. La syntaxe est la suivante : condition ? valeur_si_vrai : valeur_si_faux;
+
             if (hotel == "P")
             {
                 prixHotel = (nbJour > 7) ? (nbJour - 7) * PLAZZA_SUPP + PLAZZA_SEM : PLAZZA_SEM;
@@ -84,10 +103,13 @@ namespace Exo5_Voyage
                 }
             }
 
-            prixTotal = (prixHotel + prixVoiture);
+            //On calcule le prix total du voyage en additionnant le prix de l'hotel et de la voiture, puis on affiche le résultat avec un format monétaire
 
+            prixTotal = (prixHotel + prixVoiture);
             Console.WriteLine($"Votre voyage vous coutera au total {prixTotal:C}, pour {prixHotel:C} d'hôtel et {prixVoiture:C} de location de voiture.");
 
+            //Rappel, le fromat monétaire est défini par la culture de l'ordinateur, donc si vous êtes en Suisse, le format sera CHF, si vous êtes en France, le format sera €, etc.
+            //Il s'écrit avec un entête "$" sur le string, puis "{valeur:C}" dans le string pour indiquer le format monétaire.
 
         }
     }
