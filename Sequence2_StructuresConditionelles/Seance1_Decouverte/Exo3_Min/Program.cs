940e083bc15d6c245a1480b5116c6ab644713598
NormandEwann <ewann.normand@etu.univ-smb.fr>
2026-09-19 15:12:11 +0200

    Update Program.cs

--- Sequence2_StructuresConditionelles/Seance1_Decouverte/Exo3_Min/Program.cs
+++ Sequence2_StructuresConditionelles/Seance1_Decouverte/Exo3_Min/Program.cs
@@ -6,22 +6,28 @@ namespace Exo3_Min
     {
         static void Main(string[] args)
         {
-            double min = 0;
+            //Creation de variables et attribution directe des valeurs
+            double minimum = 0;
             Console.WriteLine("Tapez 3 numéros :");
             double num1 = double.Parse(Console.ReadLine());
             double num2 = double.Parse(Console.ReadLine());
             double num3 = double.Parse(Console.ReadLine());
 
-            min = num1;
+            //On suppose que num1 est le plus petit
+            minimum = num1;
+            //On compare num1 avec num2 dans un premier temps, si num1 est plus grand que num2, on change la valeur de minimum pour num2
             if (num1 >= num2)
             {
-                min = num2;
+                minimum = num2;
             }
+            //On fait de meme pour num3, si num1 est plus grand que num3, on change la valeur de minimum pour num3
             if (num1 >= num3)
             {
-                min = num3;
+                minimum = num3;
             }
-            Console.WriteLine(min + " est le plus petit");
+
+            //Affichage du résultat
+            Console.WriteLine(minimum + " est le plus petit");
         }
     }
 }
