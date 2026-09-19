f3e5976bae5fd429755b4d39f53a21429c07797b
NormandEwann <ewann.normand@etu.univ-smb.fr>
2026-09-19 14:18:19 +0200

    Enhance Program.cs with explanatory comments
    Added comments to clarify variable creation, value assignment, and calculation steps.

--- Sequence1_InstructionDeBase/Seance2/Exo2_Moyenne/Program.cs
+++ Sequence1_InstructionDeBase/Seance2/Exo2_Moyenne/Program.cs
@@ -4,13 +4,22 @@
     {
         static void Main(string[] args)
         {
+            //Création des variables
             double val1 = 0, val2 = 0;
+
+            //Attribution de valeurs aux variables
             Console.WriteLine("Valeure 1 :");
             val1 = double.Parse(Console.ReadLine());
+            
             Console.WriteLine("Valeure 2 :");
             val2 = double.Parse(Console.ReadLine());
-            Console.WriteLine("------------------------");
+            
+            //Maths (Moyenne = somme des valeurs divisé par leur nombre)
+            //Attention a bien utiliser les parenthèse pour additionner
             double moy = (val1 + val2) / 2;
+
+            //Afficher le résultat
+            Console.WriteLine("------------------------");
             Console.WriteLine("La moyenne est de : " + moy + ".");
         }
     }
